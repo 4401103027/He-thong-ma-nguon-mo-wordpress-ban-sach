ff909fda4ccdf2ba72973fdb82e398694a1d7026
admin <thanhngan1482000@gmail.com>
2021-11-12 11:03:29 +0700

    gui Long

---
+++ book-Copy/wp-content/plugins/block-options/build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '3247223ea371fc7139ce22904c6529b6');
---
+++ book-Copy/wp-content/plugins/block-options/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-element', 'wp-polyfill', 'wp-wordcount'), 'version' => 'ebc9a8ec18da79a3a4279bf2e6c3e281');
